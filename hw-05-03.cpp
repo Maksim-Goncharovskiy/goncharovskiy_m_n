9f2e0f8d12f6a40c00472efbc4a92410894b6c2d
Maksim Goncharovskiy <69748746+Maksim-Goncharovskiy@users.noreply.github.com>
2022-10-03 22:03:52 +0300

    Create hw-05-03.cpp

---
+++ hw-05-03.cpp
@@ -0,0 +1,35 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+double factorial(double x) {
+	double ans = 1;
+	for (double i = 1; i <= x; i++) {
+		ans = ans * i;
+	}
+	return ans;
+}
+
+int main() {
+	const double e = M_E;
+	double a = 0;
+	double b = 1;
+	double x = a;
+	double x_n = 0; 
+	for (x = a; x <= b; x = x + 0.1) {
+		x = round(x * 10) / 10;
+		double i = 0;
+		double sum = 0;
+		double f = pow(e, 2*x);
+		while (true) {
+			x_n = std::pow(2*x, i) / factorial(i);
+			if (abs(x_n) >= pow(10.0, -4.0)) {
+				sum = sum + x_n;
+				i = i + 1;
+			}
+			else {
+				break;
+			}
+		}
+		std::cout << x << ' ' << sum << ' ' << f << std::endl;
+	}
+}
