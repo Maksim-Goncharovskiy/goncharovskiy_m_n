8634f680f58eb4d9cf683e45a96b14f7ff833cee
Maksim Goncharovskiy <69748746+Maksim-Goncharovskiy@users.noreply.github.com>
2022-10-03 22:18:07 +0300

    Create hw-05-02.cpp

---
+++ hw-05-02.cpp
@@ -0,0 +1,27 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+int main() {
+	const double pi = M_PI;
+	double a = 0;
+	double b = 1;
+	double x = a;
+	double x_n = 0; 
+	for (x = a; x <= b; x = x + 0.05) {
+		x = round(x * 100) / 100;
+		double i = 1;
+		double sum = 0;
+		double f = (x*sin(pi/4))/(1 - 2*x*cos(pi/4));
+		while (true) {
+			x_n = std::pow(x, i)*sin(i*pi/4);
+			if (abs(x_n) >= pow(10.0, -3.0)) {
+				sum = sum + x_n;
+				i = i + 1;
+			}
+			else {
+				break;
+			}
+		}
+		std::cout << x << ' ' << sum << ' ' << f << std::endl;
+	}
+}
