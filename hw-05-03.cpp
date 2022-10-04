278eb83fdefa5880a8160513c4302efa63715c9f
Maksim Goncharovskiy <69748746+Maksim-Goncharovskiy@users.noreply.github.com>
2022-10-04 14:10:19 +0300

    Update hw-05-03.cpp

--- hw-05-03.cpp
+++ hw-05-03.cpp
@@ -14,21 +14,17 @@ int main() {
 	double a = 0;
 	double b = 1;
 	double x = a;
-	double x_n = 0; 
+	double x_n = 0;
 	for (x = a; x <= b; x = x + 0.1) {
 		x = round(x * 10) / 10;
 		double i = 0;
 		double sum = 0;
-		double f = pow(e, 2*x);
-		while (true) {
-			x_n = std::pow(2*x, i) / factorial(i);
-			if (abs(x_n) >= pow(10.0, -4.0)) {
-				sum = sum + x_n;
-				i = i + 1;
-			}
-			else {
-				break;
-			}
+		double f = pow(e, 2 * x);
+		x_n = std::pow(2 * x, i) / factorial(i);
+		while (abs(x_n) >= pow(10.0, -4.0)) {
+			sum = sum + x_n;
+			i = i + 1;
+			x_n = std::pow(2 * x, i) / factorial(i);
 		}
 		std::cout << x << ' ' << sum << ' ' << f << std::endl;
 	}
