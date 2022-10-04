aebaf0579efa19411b25d8fb2c66fc52de7bb696
Maksim Goncharovskiy <69748746+Maksim-Goncharovskiy@users.noreply.github.com>
2022-10-04 21:46:27 +0300

    Update 0263a.cpp

--- goncharovskiy_m_n/0263a.cpp
+++ goncharovskiy_m_n/0263a.cpp
@@ -1,22 +1,33 @@
 //Задача - Красивая матрица
+//Идея решения: Считываем в цикле по одной строке, проверяя каждый элемент 
+//на 1. Если какой-то элемент равен 1, мы сохраняем значения его строки и столбца и выходим
+//из цикла. Затем просто считаем сколько ходов нужно сделать (abs(3-line)) + (abs(3-column))
 #include<iostream>
 int main(){
-    int year = 0;
-    int a =0;
-    int b = 0;
-    int c = 0;
-    int d = 0;
-    std::cin>>year;
-    bool flag = true;
-    while (flag){
-        year = year + 1;
-        a = year%10;
-        b = (year/10)%10;
-        c = (year/100)%10;
-        d = (year/1000)%10;
-        if (a!=b && b!=c && c!=d && a!=c && a!=d && b!=d){
-            flag = false;
-            std::cout<<year;
+    int line = 1;//номер текущей строки
+    int column = 0, a, b, c, d, e;
+    for (line; line <= 5; line++){
+        std::cin>>a>>b>>c>>d>>e;
+        if (a==1){
+            column = 1;
+            break;
+        }
+        if (b==1){
+            column = 2;
+            break;
+        }
+        if (c==1){
+            column = 3;
+            break;
+        }
+        if (d==1){
+            column = 4;
+            break;
+        }
+        if (e==1){
+            column = 5;
+            break;
         }
     }
+    std::cout<<(abs(3 - line) + abs(3 - column));
 }
