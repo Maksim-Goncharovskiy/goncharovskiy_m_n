8da520f6c040c4b502195645c9b9cb16135ead7b
Maksim Goncharovskiy <69748746+Maksim-Goncharovskiy@users.noreply.github.com>
2022-10-18 11:18:27 +0300

    Update 1692c.cpp

--- goncharovskiy_m_n/1692c.cpp
+++ goncharovskiy_m_n/1692c.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <string>
 int main() {
 	std::vector <int> moves_i;
 	std::vector <int> moves_j;
 	for (int i = 1; i <= 8; i++) {
-		for (int j = 1; j <= 8; j++) {
-			char x;
-			std::cin >> x;
-			if (x == '#') {
-				moves_i.push_back(i);
-				moves_j.push_back(j);
-			}
-		}
+		std::string s;
+		std::cin >> s;
 	}
-	for (int k = 0; k < moves_i.size(); k++) {
-		std::cout << moves_i[k] << ' ' << moves_j[k] << std::endl;
-	}
+
 }
