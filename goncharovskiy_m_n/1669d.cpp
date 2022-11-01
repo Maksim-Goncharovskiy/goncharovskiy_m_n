4d3448e760e64442ff2140b6f8a719d0bc738347
Maksim Goncharovskiy <69748746+Maksim-Goncharovskiy@users.noreply.github.com>
2022-11-01 22:13:12 +0300

    Update 1669d.cpp

--- goncharovskiy_m_n/1669d.cpp
+++ goncharovskiy_m_n/1669d.cpp
@@ -1,31 +1,55 @@
 #include <iostream>
 #include <string>
 int main() {
-	int n = 0;
-	std::string s;
-	std::cin >> s;
-	int count_B = 0;
-	int count_R = 0;
-	bool ans = true;
-	for (int i = 0; i < n; i++) {
-		if (s[i] != 'W') {
-			if (s[i] == 'B') {
-				count_B += 1;
+	int t = 0;
+	std::cin >> t;
+	for (int g = 0; g < t; g++) {
+		int n = 0;
+		std::cin >> n;
+		std::string s;
+		std::cin >> s;
+		int count_B = 0;
+		int count_R = 0;
+		bool ans = true;
+		for (int i = 0; i < n; i++) {
+			if (s[i] != 'W') {
+				if (s[i] == 'B') {
+					count_B += 1;
+				}
+				else if (s[i] == 'R') {
+					count_R += 1;
+				}
 			}
-			else if (s[i] == 'R') {
-				count_R += 1;
+			else {
+				if (count_B == 0 && count_R > 0) {
+					ans = false;
+					break;
+				}
+				else if (count_B > 0 && count_R == 0) {
+					ans = false;
+					break;
+				}
+				count_B = 0;
+				count_R = 0;
 			}
+		}
+		if (count_B + count_R > 0) {
+			if (count_B > 0 && count_R == 0) {
+				std::cout << "No" << std::endl;
+			}
+			else if (count_R > 0 && count_B == 0) {
+				std::cout << "No" << std::endl;
+			}
+			else {
+				std::cout << "Yes" << std::endl;
+			}
+		}
+		else if (ans) {
+			std::cout << "Yes" << std::endl;
 		}
 		else {
-			if (count_B == 0 && count_R > 0) {
-				ans = false;
-				break;
-			}
-			else if (count_B > 0 && count_R == 0) {
-				ans = false;
-				break;
-			}
-			else if ()
+			std::cout << "No" << std::endl;
 		}
 	}
+	
 }
