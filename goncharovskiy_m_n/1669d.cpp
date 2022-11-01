2367e977b48820e82bcb5e6498f8f97704aa278b
Maksim Goncharovskiy <69748746+Maksim-Goncharovskiy@users.noreply.github.com>
2022-11-01 11:08:03 +0300

    Update 1669d.cpp

--- goncharovskiy_m_n/1669d.cpp
+++ goncharovskiy_m_n/1669d.cpp
@@ -1,63 +1,31 @@
-#include<string>
 #include <iostream>
-#include <vector>
+#include <string>
 int main() {
-	int t = 0;
-	std::cin >> t;
-	for (int g = 0; g < t; g++) {
-		int n = 0;
-		std::string s;
-		std::cin >> n;
-		std::cin >> s;
-		std::string local_s = "";
-		std::vector <std::string> A;
-		if (s.find('R') >= n && s.find('B') >= n) {
-			std::cout << "Yes" << std::endl;
-		}
-		else{
-			for (int i = 0; i < n; i++) {
-				char x = s[i];
-				if (x != 'W') {
-					if (x == 'R') {
-						local_s = local_s + "R";
-					}
-					else {
-						local_s = local_s + "B";
-					}
-				}
-				else {
-					if (local_s != ""){
-						A.push_back(local_s);
-						local_s = "";
-					}
-				}
-				if (i == n - 1 && s[i] != 'W') {
-					A.push_back(local_s);
-				}
+	int n = 0;
+	std::string s;
+	std::cin >> s;
+	int count_B = 0;
+	int count_R = 0;
+	bool ans = true;
+	for (int i = 0; i < n; i++) {
+		if (s[i] != 'W') {
+			if (s[i] == 'B') {
+				count_B += 1;
 			}
-			//for (int k = 0; k < A.size(); k++) {
-				//std::cout << A[k] << ' ';
-			//}
-			bool ans = true;
-			for (int j = 0; j < A.size(); j++) {
-				std::string str = A[j];
-				if (str.size() <= 1) {
-					ans = false;
-					break;
-				}
-				else if (str.find('R') >= str.size() || str.find('B') >= str.size()) {
-					ans = false;
-					break;
-				}
-			}
-			if (ans) {
-				std::cout << "Yes"<<std::endl;
-			}
-			else {
-				std::cout << "No"<<std::endl;
+			else if (s[i] == 'R') {
+				count_R += 1;
 			}
 		}
-
+		else {
+			if (count_B == 0 && count_R > 0) {
+				ans = false;
+				break;
+			}
+			else if (count_B > 0 && count_R == 0) {
+				ans = false;
+				break;
+			}
+			else if ()
+		}
 	}
-
 }
