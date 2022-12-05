755a68dcbaa45f7383e69ebfc98fa04d51fd8739
Maksim Goncharovskiy <69748746+Maksim-Goncharovskiy@users.noreply.github.com>
2022-12-05 22:55:29 +0300

    Create vectors.h

---
+++ goncharovskiy_m_n/vectors_lib/vectors.h
@@ -0,0 +1,20 @@
+#pragma once
+
+struct Rdec2D { 
+	double x = 0.0;
+	double y = 0.0; 
+};
+
+struct Rpd2D {
+	double r = 0.0;
+	double phi = 0.0;
+};
+
+Rdec2D perevod1(Rpd2D n);
+Rpd2D perevod2(Rdec2D n);
+double lengh(Rdec2D n);
+Rdec2D operator+(Rdec2D n, Rdec2D m);
+Rdec2D operator-(Rdec2D n, Rdec2D m);
+Rdec2D operator*(Rdec2D n, double c);
+double dot(Rdec2D n, Rdec2D m);
+void print_Rdec(Rdec2D n);
