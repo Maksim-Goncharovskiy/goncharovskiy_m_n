03b5076e9f7e4fca5cdbe1a93ffb2fd90be9bd15
Maksim Goncharovskiy <69748746+Maksim-Goncharovskiy@users.noreply.github.com>
2022-12-09 13:29:14 +0300

    Create modelling.cpp

---
+++ goncharovskiy_m_n/vectors_lib/modelling.cpp
@@ -0,0 +1,28 @@
+#include "vectors.h"
+#include <iostream>
+#include <cmath>
+#include <fstream>
+int main() {
+	struct jojo 
+	{
+		Rdec2D r = {0, 10};
+		Rdec2D v = {0.3953, 0};
+		Rdec2D a = {0, 0};
+	};
+	std::fstream file;
+	file.open("data.csv");
+	file.clear();
+	double const c = 1;
+	jojo point;
+	Rdec2D g;
+	Rdec2D& a1 = point.a;
+	Rdec2D& r1 = point.r;
+	Rdec2D& v1 = point.v;
+	double t = 1;
+	for (int i = 0; i < 1000; i++) {
+		g = r1 * (-1) * (1 / lengh(r1)) * (1 / pow(lengh(r1), 2));
+		v1 = v1 + g * t + a1 * t;
+		r1 = r1 + v1 * t;
+		file << r1.x <<";"<<r1.y<<std::endl;
+	}
+}
