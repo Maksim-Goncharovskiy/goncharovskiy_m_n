16840426b4d04f5a7da5cea596772904cf9f5bd9
Maksim Goncharovskiy <69748746+Maksim-Goncharovskiy@users.noreply.github.com>
2022-12-13 00:26:17 +0300

    Update cat.cpp

--- goncharovskiy_m_n/vectors_lib/cat.cpp
+++ goncharovskiy_m_n/vectors_lib/cat.cpp
@@ -1,29 +1,42 @@
+#define _USE_MATH_DEFINES
 #include "vectors.h"
 #include <iostream>
 #include <cmath>
-int main() {
-  Rdec2D r_c = {5, 0};
-	Rdec2D r_m = { -1, -8 };
-	Rdec2D v_m = { 0, -100 };
-	Rdec2D v_c = (r_m - r_c) * (5 / lengh(r_m - r_c));
-	
-	double Radius = 2;
-	std::string ans;
-	print_Rdec(v_c);
-	
-	while ((r_m.y != 0) && (pow(r_m.x - r_c.x, 2) + pow(r_m.y - r_c.y, 2)) > pow(Radius, 2)) {
-		r_m = r_m + v_m;
-		r_c = r_c + v_c;
-		Rdec2D v_c = (r_m - r_c) * (1 / lengh(r_m - r_c));
-		if (r_m.y == 0) {
-			ans = "Mouse will be alive";
-		}
-		else if ((pow(r_m.x - r_c.x, 2) + pow(r_m.y - r_c.y, 2)) <= pow(Radius, 2)) {
-			ans = "Cat had a nice dinner";
-		}
-	}
-	if (ans == "") {
-		ans = "Mouse really fast";
-	}
-	std::cout << ans;
+#include <iomanip>
+
+int main(Rdec2D r_m, Rdec2D r_c, Rdec2D v_m, Rdec2D v_c, double CatRad, double v0) {
+	Rdec2D r_m;
+	Rdec2D r_c;
+	Rdec2D v_c;
+	double radius = 3; 
+    
+    	double last;
+    	last = norm(r_m - r_c);
+    	double dt = 0.01;
+    	double count_t = 0;
+    	
+	r_m = r_m + v_m * dt;
+    	r_c = r_c + v_c * dt;
+    	v_c = (r_m - r_c) * (1 / norm(r_m - r_c)) * v0;
+    	count_t += dt;
+
+    	while ((r_m.y < 0) && (norm(r_m - r_c) > radius)) {
+        	if (last < norm(r_m - r_c) && v_m.y < 0)
+            	break;
+       		last = norm(r_m - r_c);
+        	count_t += dt;
+        	r_m = r_m + v_m * dt;
+        	r_c = r_c + v_c * dt;
+        	v_c = (r_m - r_c) * (1 / norm(r_m - r_c)) * v0;
+    	}
+    	if (r_m.y >= 0) {
+        	std::cout << "Mouse in Hole in " << count_t << " second\n";
+    	}
+    	else if (norm(r_m - r_c) <= CatRad) {
+        	std::cout << "Cat ate Mouse in " << count_t << " second\n";
+    	}
+    	else if (last <= norm(r_m - r_c)) {
+        	std::cout << "Mouse ran away \n";
+    	}
+    	return;
 }
