855fcd7d826f29629747024dc22fa3c658514a21
Maksim Goncharovskiy <69748746+Maksim-Goncharovskiy@users.noreply.github.com>
2022-12-16 19:11:15 +0300

    Update cat-n-mouse(1).cpp

--- goncharovskiy_m_n/cat-n-mouse(1).cpp
+++ goncharovskiy_m_n/cat-n-mouse(1).cpp
@@ -9,29 +9,29 @@ int main() {
     double radius = 1;
 
     double prev_distance; //Расстояние между котом и мышью. Будем считать за первоначальное расстояние, а потом смотреть как оно будет меняться
-    prev_distance = lengh(r_m - r_c);
+    prev_distance = Norm(r_m - r_c);
     double t = 0.01;
 
     r_m = r_m + v_m * t;
     r_c = r_c + v_c * t;
-    v_c = (r_m - r_c) * (1 / lengh(r_m - r_c));
+    v_c = (r_m - r_c) * (1 / Norm(r_m - r_c));
 
-    while ((r_m.y < 0) && (lengh(r_m - r_c) > radius)) {
-        if (prev_distance < lengh(r_m - r_c) && v_m.y < 0) { //случай когда мышь убегает
+    while ((r_m.y < 0) && (Norm(r_m - r_c) > radius)) {
+        if (prev_distance < Norm(r_m - r_c) && v_m.y < 0) { //случай когда мышь убегает
             break;
         }
-        prev_distance = lengh(r_m - r_c);
+        prev_distance = Norm(r_m - r_c);
         r_m = r_m + v_m * t;
         r_c = r_c + v_c * t;
-        v_c = (r_m - r_c) * (1 / lengh(r_m - r_c));
+        v_c = (r_m - r_c) * (1 / Norm(r_m - r_c));
     }
     if (r_m.y >= 0) {
         std::cout << "Mouse will be alive"<< std::endl;
     }
-    else if (lengh(r_m - r_c) <= radius) {
+    else if (Norm(r_m - r_c) <= radius) {
         std::cout << "Cat had a nice dinner"<< std::endl;
     }
-    else if (prev_distance <= lengh(r_m - r_c)) {
+    else if (prev_distance <= Norm(r_m - r_c)) {
         std::cout << "Mouse really fast" << std::endl;
     }
 }
